fc51b8e126aa4600cf5405f4a30c2d2c75f8991a
Pablo Renato <pablorenato18@gmail.com>
2024-06-03 01:24:54 -0300

    setting the class pessoa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using construindo_um_sistema_de_hospedagem_de_um_hotel.models;
+
+Pessoa p1 = new Pessoa(nome: "Pablo");
+Pessoa p2 = new Pessoa(nome)
+Console.WriteLine(p1.Nome);
--- models/Pessoa.cs
+++ models/Pessoa.cs
@@ -7,6 +7,20 @@ namespace construindo_um_sistema_de_hospedagem_de_um_hotel.models
 {
     public class Pessoa
     {
+        public Pessoa() { }
+        public Pessoa(string nome) 
+        {
+            Nome = nome;
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome; 
+            Sobrenome = sobrenome; 
+        }
         
+        public string Nome {get; set;}
+        public string Sobrenome {get; set;} 
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
     }
 }
