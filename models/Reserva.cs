98bf39af66d3b3851e1b4f298bce49982fe44e86
Pablo Renato <pablorenato18@gmail.com>
2024-06-03 01:07:13 -0300

    setup the files

---
+++ models/Reserva.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace construindo_um_sistema_de_hospedagem_de_um_hotel.models
+{
+    public class Reserva
+    {
+        
+    }
+}
