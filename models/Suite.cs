7bb33d82291b40cec68dfe3328359eeccf259646
Pablo Renato <pablorenato18@gmail.com>
2024-06-03 01:28:45 -0300

    Setting Suite class

--- models/Suite.cs
+++ models/Suite.cs
@@ -7,6 +7,8 @@ namespace construindo_um_sistema_de_hospedagem_de_um_hotel.models
 {
     public class Suite
     {
-        
+        string TipoSuite { get; set; }
+        int Capacidade { get; set; }
+        decimal ValorDiaria {get;set;}
     }
 }
